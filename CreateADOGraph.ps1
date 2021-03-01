79db6b56faa3bc2b97449a51503bb0306e752f44
Adam Jagocki <ajagocki@hotmail.com>
2021-03-01 16:26:55 +0100

    fixed install script

--- CreateADOGraph.ps1
+++ CreateADOGraph.ps1
@@ -1,15 +1,16 @@
 #credits to https://github.com/KevinMarquette/PSGraph
 
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install graphviz
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Find-Module PSGraph | Install-Module
+
+# alternative way
 # Install GraphViz from the Chocolatey repo
 # Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/
 # Find-Package graphviz | Install-Package -ForceBootstrap
 
-# Install PSGraph from the Powershell Gallery
-#Find-Module PSGraph | Install-Module
-
-# Install-Module -Name PSGraph -RequiredVersion 2.1.35
-# Import Module
-# choco install graphviz
 Import-Module PSGraph
 
 function CreateGraph {
